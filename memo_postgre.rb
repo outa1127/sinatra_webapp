22fb63456775cb0295b1899f2fb29f16074f3d84
outa1127 <ota.ono@livesense.co.jp>
2025-05-14 17:28:44 +0900

    削除、変更機能を追加

--- memo_postgre.rb
+++ memo_postgre.rb
@@ -15,17 +15,13 @@ class Memo
     @content = content
   end
 
-  def self.load_memos
-    JSON.parse(File.read(JSON_FILE_PATH), symbolize_names: true)
-  end
-
   def self.connect_postgresql
     PG::Connection.new(dbname: ENV['DB_NAME'], user: ENV['DB_USER'], host: ENV['DB_HOST'], port: ENV['DB_PORT'])
   end
 
   def self.all
     conn = connect_postgresql
-    all_memos = conn.exec('SELECT * FROM memos;')
+    all_memos = conn.exec('SELECT * FROM memos ORDER BY id ASC;')
     all_memos.map do |memo|
       new(
         id: memo['id'],
@@ -40,7 +36,6 @@ class Memo
     # [{"id" => "1   ", "title" => "test1", "content" => "test1の内容"}]みたいな形で格納されている
     # そのため、.firstでハッシュを直接memo_dataに格納するようにしている
     memo_data = conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id]).first
-    p memo_data
     return nil unless memo_data
 
     new(
@@ -51,32 +46,17 @@ class Memo
   end
 
   def self.create(title:, content:)
-    memos = load_memos
-    max_id = memos.map { |memo| memo[:id] }.max || 0
-    new_memo = {
-      id: max_id + 1,
-      title: title,
-      content: content
-    }
-    memos << new_memo
-    save_memos(memos)
+    conn = connect_postgresql
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
   end
 
   def update(title:, content:)
-    memos = self.class.load_memos
-    memo = memos.find { |memo| memo[:id] == @id }
-    memo[:title] = title
-    memo[:content] = content
-    self.class.save_memos(memos)
+    conn = self.class.connect_postgresql
+    conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, @id])
   end
 
   def delete
-    memos = self.class.load_memos
-    memos.reject! { |memo| memo[:id] == @id }
-    self.class.save_memos(memos)
-  end
-
-  def self.save_memos(memos)
-    File.write(JSON_FILE_PATH, JSON.pretty_generate(memos))
+    conn = self.class.connect_postgresql
+    conn.exec_params('DELETE FROM memos WHERE id = $1', [@id])
   end
 end
